302400cfe9eb391d32bb10f66a9cad9835d4cefe
Jitendra Adhikari <jiten.adhikary@gmail.com>
2015-09-03 12:48:56 +0545

    trim
    trim and clean up the mess

--- remittance/cli-config.php
+++ remittance/cli-config.php
@@ -17,40 +17,18 @@ $em = function () use ($creds) {
         return $_em;
     }
 
-    // Register annot
-    \Doctrine\Common\Annotations\AnnotationRegistry::registerFile(
-        './vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php'
-    );
-
+    $isDevMode = true;
     $cache = new \Doctrine\Common\Cache\ArrayCache();
-    $cachedAnnotationReader = new \Doctrine\Common\Annotations\CachedReader(
-        new \Doctrine\Common\Annotations\AnnotationReader(),
-        $cache
+    $config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration(
+        [__DIR__.'/src/Entity'],
+        $isDevMode,
+        __DIR__.'/src/Proxy'
     );
-    $annotationDriver = new Doctrine\ORM\Mapping\Driver\AnnotationDriver(
-        $cachedAnnotationReader,
-        ['./src/Entity/']
-    );
-
-    $driverChain = new Doctrine\ORM\Mapping\Driver\DriverChain();
-    $driverChain->addDriver($annotationDriver, 'Entity');
-
-    $config = new \Doctrine\ORM\Configuration();
-    $config->setProxyDir('./src/Proxy');
-    $config->setProxyNamespace('Proxy');
-    // $config->setAutoGenerateProxyClasses(true);
-    $config->setMetadataDriverImpl($driverChain);
-    $config->setMetadataCacheImpl($cache);
-    $config->setQueryCacheImpl($cache);
 
     return $_em = \Doctrine\ORM\EntityManager::create(
         $creds,
-        $config,
-        new \Doctrine\Common\EventManager()
+        $config
     );
 };
 
-return new \Symfony\Component\Console\Helper\HelperSet([
-    'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em()->getConnection()),
-    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em()),
-]);
+return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($em());
